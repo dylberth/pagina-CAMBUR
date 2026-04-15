535e0f6df3c4bf773d0f8f3bf346087f7937f624
Dylberth Rômullo <Dylberthpinheiro@gmail.com>
2026-04-15 21:46:37 +0000

    Configurar basePath condicionalmente por ambiente

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/pagina-CAMBUR',
-  assetPrefix: '/pagina-CAMBUR/',
+  basePath: isProduction ? '/pagina-CAMBUR' : '',
+  assetPrefix: isProduction ? '/pagina-CAMBUR/' : '',
 };
 
 module.exports = nextConfig;
