2da5c619d905a004faff7b986a4833ee1d74bf33
Dylberth Rômullo <Dylberthpinheiro@gmail.com>
2026-04-15 15:40:06 +0000

    correção layout

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -4,10 +4,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
-      <body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>
-        {children}
-      </body>
-    </html>
-  );
+<html lang="pt-BR">
+<body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>
+{children}
+</body>
+</html>
 }
